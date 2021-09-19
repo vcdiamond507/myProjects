37a11ae5a5e951e2767d99ef8f9288e5a745926e
vcdiamond507 <vcdiamond507@gmail.com>
2021-09-19 22:45:11 +0530

    started

---
+++ Lightning-Bulb/bulb.js
@@ -0,0 +1,8 @@
+function onoff()
+{
+    var onoff = document.querySelector('.onoffbtn');
+    onoff.classList.toggle('clickbtn');
+
+    var glow = document.querySelector('.container');
+    glow.classList.toggle('glow');
+}
